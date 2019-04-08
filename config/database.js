f2105f8a2feab78bd0482c4c23ec187a447dbdb5
kikichau <kiki.1155@hotmail.com>
2019-04-08 23:24:32 +0800

    Add database password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb+srv://kikichau:<password>@web-api-4azln.mongodb.net/test?retryWrites=true';
+const mongoDB = 'mongodb+srv://kikichau:00000000@web-api-4azln.mongodb.net/test?retryWrites=true';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
