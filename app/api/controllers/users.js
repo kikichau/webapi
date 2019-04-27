29272a37d4bfe159189256e20f2f80bc197706f3
kikichau <kiki.1155@hotmail.com>
2019-04-27 23:17:56 +0800

    Update user controllers

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -3,13 +3,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 module.exports = {
    create: function (req, res, next) {
-
       userModel.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function (err, result) {
          if (err)
             next(err);
          else
             res.json({ status: "success", message: "User added successfully!!!", data: null });
-
       });
    },
    authenticate: function (req, res, next) {
