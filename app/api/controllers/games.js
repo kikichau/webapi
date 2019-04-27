970d37b26d377ab8ca08c71817ab8688431105a6
kikichau <kiki.1155@hotmail.com>
2019-04-27 23:15:55 +0800

    Update game controllers

--- app/api/controllers/games.js
+++ app/api/controllers/games.js
@@ -1,54 +1,52 @@
 const gameModel = require('../models/games');
 module.exports = {
- getById: function(req, res, next) {
-  console.log(req.body);
-  gameModel.findById(req.params.gameId, function(err, gameInfo){
-   if (err) {
-    next(err);
-   } else {
-    res.json({status:"success", message: "Game found!!!", data:{games: gameInfo}});
-   }
-  });
- },
-getAll: function(req, res, next) {
-  let gamesList = [];
-gameModel.find({}, function(err, games){
-   if (err){
-    next(err);
-   } else{
-    for (let game of games) {
-     gamesList.push({id: game._id, name: game.name, released_on: game.released_on});
-    }
-    res.json({status:"success", message: "Games list found!!!", data:{gmaes: gamesList}});
-       
-   }
-});
- },
-updateById: function(req, res, next) {
-  gameModel.findByIdAndUpdate(req.params.gameId,{name:req.body.name}, function(err, gameInfo){
-if(err)
-    next(err);
-   else {
-    res.json({status:"success", message: "Game updated successfully!!!", data:null});
-   }
-  });
- },
-deleteById: function(req, res, next) {
-  gameModel.findByIdAndRemove(req.params.gameId, function(err, gameInfo){
-   if(err)
-    next(err);
-   else {
-    res.json({status:"success", message: "Game deleted successfully!!!", data:null});
-   }
-  });
- },
-create: function(req, res, next) {
-  gameModel.create({ name: req.body.name, released_on: req.body.released_on }, function (err, result) {
-      if (err) 
-       next(err);
+  create: function (req, res, next) {
+    gameModel.create({ name: req.body.name, game_information: req.body.game_information, released_on: req.body.released_on, developer: req.body.developer }, function (err, result) {
+      if (err)
+        next(err);
       else
-       res.json({status: "success", message: "Game added successfully!!!", data: null});
-      
+        res.json({ status: "success", message: "Game added successfully!!!", data: null });
     });
- },
+  },
+  updateById: function (req, res, next) {
+    gameModel.findByIdAndUpdate(req.params.gameId, { name: req.body.name, game_information: req.body.game_information, released_on: req.body.released_on, developer: req.body.developer }, function (err, gameInfo) {
+      if (err)
+        next(err);
+      else {
+        res.json({ status: "success", message: "Game updated successfully!!!", data: null });
+      }
+    });
+  },
+  deleteById: function (req, res, next) {
+    gameModel.findByIdAndRemove(req.params.gameId, function (err, gameInfo) {
+      if (err)
+        next(err);
+      else {
+        res.json({ status: "success", message: "Game deleted successfully!!!", data: null });
+      }
+    });
+  },
+  getAll: function (req, res, next) {
+    let gamesList = [];
+    gameModel.find({}, function (err, games) {
+      if (err) {
+        next(err);
+      } else {
+        for (let game of games) {
+          gamesList.push({ id: game._id, name: game.name, game_information: game.game_information, released_on: game.released_on, developer: game.developer });
+        }
+        res.json({ status: "success", message: "Games list found!!!", data: { gmaes: gamesList } });
+      }
+    });
+  },
+  getById: function (req, res, next) {
+    console.log(req.body);
+    gameModel.findById(req.params.gameId, function (err, gameInfo) {
+      if (err) {
+        next(err);
+      } else {
+        res.json({ status: "success", message: "Game found!!!", data: { games: gameInfo } });
+      }
+    });
+  },
 }
